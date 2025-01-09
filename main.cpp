46a4ae74c9cade095d9c04ee7f6af31ae52f0492
janvaliser <jan.valiser@gmail.com>
2025-01-09 23:02:52 +0100

    [DD-333] Add OpenMPI

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
-int main()
+#include <mpi.h>
+
+int main(const int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // M P I
+    int rank;
+    int numberOfProcesses;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    printf("Hello world from processor %s, rank %d out of %d processors\n", processor_name);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
 }
